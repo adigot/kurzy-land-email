80b597c65fd99063f005964fc2051c5da84ca9bd
adigot <adela.gottwaldova@gmail.com>
2024-10-19 21:35:03 +0000

    Use ES moudle export

--- netlify/functions/submit-form.js
+++ netlify/functions/submit-form.js
@@ -1,6 +1,6 @@
 import sgMail from '@sendgrid/mail';
 
-exports.handler = async (event, context) => {
+export const handler = async (event, context) => {
   if (event.httpMethod !== 'POST') {
     return { statusCode: 405, body: 'Method Not Allowed' };
   }
