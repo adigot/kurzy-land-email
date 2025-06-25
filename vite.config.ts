e7e00992a011256ecbc629806f046f8a6669c37b
null <null>
2025-06-25 23:18:18 +0200

    Support multiple pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        "kdo-jsme": resolve(__dirname, 'kdo-jsme.html'),
+        kontakty: resolve(__dirname, 'kontakty.html'),
+        kurzy: resolve(__dirname, 'kurzy.html'),
+      },
+    },
+  },  
   server: {
     proxy: {
       '/api': {
